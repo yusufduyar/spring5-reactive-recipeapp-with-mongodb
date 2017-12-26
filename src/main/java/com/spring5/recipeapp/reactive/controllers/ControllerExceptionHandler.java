dee00bf06886c7f850320daa29b7534d50876f2f
yusufduyar <yusufduyar@outlook.com>
2017-12-26 19:31:44 +0300

    Added controller advice for binding exceptions in a reactive way

--- src/main/java/com/spring5/recipeapp/reactive/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/spring5/recipeapp/reactive/controllers/ControllerExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.spring5.recipeapp.reactive.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
@@ -18,4 +23,15 @@ public class ControllerExceptionHandler {
 //
 //        return modelAndView;
 //    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleBindingException(Exception exception, Model model){
+        log.error("Handling binding exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception",exception);
+
+        return "400error";
+    }
 }
