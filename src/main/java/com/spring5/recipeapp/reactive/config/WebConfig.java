0d58f03db969dcc904b0fbcd77c0566cf6821774
yusufduyar <yusufduyar@outlook.com>
2017-12-26 19:44:29 +0300

    Added a sample router function

---
+++ src/main/java/com/spring5/recipeapp/reactive/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.spring5.recipeapp.reactive.config;
+
+import com.spring5.recipeapp.reactive.domain.Recipe;
+import com.spring5.recipeapp.reactive.services.IRecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(IRecipeService recipeService) {
+        return RouterFunctions.route(GET("/api/recipes"),
+                request -> ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(recipeService.getRecipes(), Recipe.class));
+
+    }
+}
